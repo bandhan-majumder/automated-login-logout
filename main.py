f536be97e9c37babe0d8d8e5af38b3d4751e6b21
bandhan-majumder <bandhanmajumderlearner@gmail.com>
2024-01-19 00:38:21 +0530

    Added ss feature at every step as a proof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import time
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -10,6 +12,8 @@ driver = webdriver.Chrome()
 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 time.sleep(2) # 2 sec time is given to load the page totally
 
+if not os.path.exists('autoSS'): # creating folder to store images
+ os.mkdir('autoSS')
 
 try:
  # logging in process:-
@@ -21,10 +25,14 @@ try:
  log_in_password=driver.find_element(By.NAME,"password")
  log_in_password.send_keys("admin123") # password : admin123
 
+ driver.get_screenshot_as_file('autoSS/loginpage.png')  # screenshot 1 : login page
+
  log_in_username.send_keys(Keys.ENTER)  # clicking enter to log in
  print("Logged in successfully!")
 
- time.sleep(3)
+ time.sleep(4)
+
+ driver.get_screenshot_as_file('autoSS/loginSucc.png') # screenshot 2 : dashboard
 
  # log out process:-
 
@@ -37,8 +45,16 @@ try:
  log_out.send_keys(Keys.ENTER) # clicking and signing out ......
  time.sleep(2)
 
+ driver.get_screenshot_as_file('autoSS/loggedout.png') # screenshot 3 : logged out and back again to log in page
  driver.quit()
  print("Logged out successfully!")
-except Exception as e:
-    print("Authentication error occurred!")
+
+except Exception as e: # if we get an error in the process, an error image will be saved
+ response = requests.get(
+  'https://developers.google.com/static/maps/documentation/maps-static/images/error-image-generic.png')
+ if response.status_code==200: # if the http request was successful
+  file_name = os.path.join('autoSS/', 'error.png')
+  with open(file_name, 'wb') as file:
+   file.write(response.content)
+ print("Authentication error occurred!")
 
